ba81699368626265c3ac8cd38ad854e223cce805
吴吴吴 <1139689957@qq.com>
2019-02-13 11:48:37 +0800

    Update center.js

--- swt/center.js
+++ swt/center.js
@@ -68,8 +68,8 @@ function closeCenter(){
 	swt_center_box.style.display = 'none';
 	//document.removeEventListener("touchmove", defaultEvent);
 	
-	//setTimeout(afterLoading,10000)
-	setTimeout(afterLoading,10000)
+	//setTimeout(afterLoading,6000)
+	setTimeout(afterLoading,6000)
 }
 
 
